a1521f817b2aa8ac84cc61cb1ea0283179e510b6
ComunicacionWeb <comunicacionweb.rcj@gmail.com>
2019-05-27 11:59:52 -0500

    mensajes en consola del navegador

--- alertas.js
+++ alertas.js
@@ -2,3 +2,6 @@ alert("alerta con JS"); //muestra primero la alerta y despues carga el contenido
 alert("segundo alert con js");
 
 document.write("Mensaje desde fichero externo");//se muestra al inicio de la página web como texto normal
+
+console.log("mostrar este mensaje en la consola"); //para mostrar msjs en la consola
+console.log(88+4);
