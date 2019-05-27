8bc18f57c9074d43fe71eba5ebd0f6a6eb62c397
ComunicacionWeb <comunicacionweb.rcj@gmail.com>
2019-05-27 11:36:05 -0500

    mensaje desde fichero externo

--- alertas.js
+++ alertas.js
@@ -1,2 +1,4 @@
 alert("alerta con JS"); //muestra primero la alerta y despues carga el contenido
 alert("segundo alert con js");
+
+document.write("Mensaje desde fichero externo");//se muestra al inicio de la página web como texto normal
