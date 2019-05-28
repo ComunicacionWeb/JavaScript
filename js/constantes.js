aa54b89b74606fe07ac44645597a3639baf62be6
ComunicacionWeb <comunicacionweb.rcj@gmail.com>
2019-05-28 11:36:33 -0500

    tipos de datos y operadores

---
+++ js/constantes.js
@@ -0,0 +1,12 @@
+"use strict"
+
+//CONSTANTES
+//es un contenedor de datos al igual que una variable, solo que el valor no puede ser modificado
+
+
+var web ="https://github.com/ComunicacionWeb/JavaScript.git";
+const ip = "192.168.0.39";
+
+web ="https://republicacentrojoyero.com",
+ip ="45566";
+console.log(web,ip);
---
+++ js/tipos_datos.js
@@ -0,0 +1,22 @@
+'use strict'
+
+//OPERADORES
+var numero1 = 7;
+var numero2 = 12;
+var operacion = numero1 * numero2;
+
+alert("El resultado de la operación es: "+operacion);
+
+
+//TIPOS DE DATOS
+var num_entero = 44;
+var cadena_texto = 'Hola "que" tal';
+var verdadero_falso = false;   //verdadero, falso, 0, 1
+
+var num_falso = "33";
+/* 	Number(numero_falso)
+	parseInt(numero_falso) // para convertir a numero entero
+	parseFloat(numero_falso) //para convertir a numero flotante(decimal)
+	//estas funciones sirven para convertir el numero que esta en string a un valor numerico
+*/
+console.log(String(num_entero)+7);	//convierte la variable en una cadena de texto y concatena con el numero
