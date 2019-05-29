009c3cd676f4aaff6ac3ff9f191778b0e55bdb93
ComunicacionWeb <comunicacionweb.rcj@gmail.com>
2019-05-29 13:41:18 -0500

    Typeof

--- js/tipos_datos.js
+++ js/tipos_datos.js
@@ -19,4 +19,10 @@ var num_falso = "33";
 	parseFloat(numero_falso) //para convertir a numero flotante(decimal)
 	//estas funciones sirven para convertir el numero que esta en string a un valor numerico
 */
-console.log(String(num_entero)+7);	//convierte la variable en una cadena de texto y concatena con el numero
+//console.log(String(num_entero)+7);	//convierte la variable en una cadena de texto y concatena con el numero
+
+
+console.log(typeof num_entero);	//typeof es para ver el tipo de dato que es la variable consultada
+console.log(typeof cadena_texto);
+console.log(typeof verdadero_falso);
+console.log(typeof num_falso);
